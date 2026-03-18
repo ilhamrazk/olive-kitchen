0579985807cfff8c6a2c3c1e09902caeac82080a
Ilham Harazki <ilhamharazky@rocketmail.com>
2026-03-19 03:37:09 +0700

    Initial Commit for Olive Kitchen Project Service

---
+++ src/main/java/com/olive/kitchen/OliveKitchenApplication.java
@@ -0,0 +1,13 @@
+package com.olive.kitchen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OliveKitchenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OliveKitchenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/olive/kitchen/OliveKitchenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.olive.kitchen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OliveKitchenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
